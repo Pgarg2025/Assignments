d78b19c57265847f72fe21dc0c31b26d31f59086
Pgarg2025 <97442147+Pgarg2025@users.noreply.github.com>
2022-01-14 15:08:17 +0530

    Add files via upload
    Python assignment - 1

---
+++ outputassign1.py
@@ -0,0 +1,38 @@
+Question 1
+Enter value of a =6
+Enter value of b =7
+Enter value of c =9
+Average is = 7.333333333333333
+
+
+
+Question 2
+Enter your gross income =100000
+Enter your no of dependents =5
+Your income tax is = 15000.0    
+
+
+
+Question 3
+Enter your SID =21103103
+Enter your name =Piyush Garg
+Enter your gender =M
+Enter your course name =CSE
+Enter your CGPA =8.9
+[21103103, 'Piyush Garg', 'M', 'CSE', 8.9]
+
+
+
+Question 4
+Enter marks of first student=67
+Enter marks of second student=92
+Enter marks of third student=53
+Enter marks of fourth student=39
+Enter marks of fifth student=71
+[39, 53, 67, 71, 92]
+
+
+
+Question 5
+Part a : ['Red', 'Green', 'White', 'Pink', 'Yellow']
+Part b : ['Red', 'Green', 'White', 'Purple', 'Yellow']
