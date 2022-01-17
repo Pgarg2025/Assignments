1486851dc70e182bc49c034ea65cc1a240ec2dd3
Pgarg2025 <97442147+Pgarg2025@users.noreply.github.com>
2022-01-17 16:32:36 +0530

    Update outputassign2.py

--- outputassign2.py
+++ outputassign2.py
@@ -19,7 +19,7 @@ I am from CSE department and my CGPA is 9.9
 
 
 
-#Question 3
+#Output 3
 Part a: 8
 Part b: 58    
 Part c: 50    
@@ -30,7 +30,7 @@ Part e, b: 0
 
 
 
-#Question 4
+#Output 4
 Enter a=45
 Enter b=79
 Enter c=56
@@ -38,14 +38,14 @@ b is greatest
 
 
 
-#Question 5
+#Output 5
 Enter any line or paragraph : 
 My name is Piyush garg.
 Yes
 
 
 
-#Question 6
+#Output 6
 Enter first side=4
 Enter second side=7
 Enter third side=12
