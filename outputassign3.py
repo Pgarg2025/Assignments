31782db042e1fbc34869c9941399421000a6f79a
Pgarg2025 <97442147+Pgarg2025@users.noreply.github.com>
2022-02-14 18:36:46 +0530

    Add files via upload

---
+++ outputassign3.py
@@ -0,0 +1,68 @@
+#Output 1
+ENTER ANY STRING: Hello my name is piyush
+{'Hello': 1, 'my': 1, 'name': 1, 'is': 1, 'piyush': 1}
+
+
+#Output 2
+ENTER THE DAY: 14
+ENTER THE MONTH OF THE YEAR: 2
+ENTER THE YEAR: 2022
+15 / 2 / 2022
+
+
+#Output 3
+Enter elements of a list separated by space 12 25 37 51
+list:  12 25 37 51
+[(12, 144), (25, 625), (37, 1369), (51, 2601)]
+
+
+#Output 4
+Enter Grade Point: 9
+Your Grade is 'A' and Excellent performance
+
+
+#Output 5
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+
+#Output 6
+Enter SID: 21103103
+Enter Name: piyush garg
+Do you want to enter another student entry(Y or N):y
+Enter SID: 21103108
+Enter Name: manik bansal
+Do you want to enter another student entry(Y or N):y
+Enter SID: 21103112
+Enter Name: tanish kalra
+Do you want to enter another student entry(Y or N):n
+students details stored in the dictionary is: {21103103: 'piyush garg', 21103108: 'manik bansal', 21103112: 'tanish kalra'}
+Sort dictionary using student names is : {21103103: 'piyush garg', 21103108: 'manik bansal', 21103112: 'tanish kalra'}
+Sort dictionary using SID is : {21103103: 'piyush garg', 21103108: 'manik bansal', 21103112: 'tanish kalra'}
+Search Name with SID (Enter sid): 21103103
+The name of student is : piyush garg
+
+
+#Output 7
+Enter the number of terms in series 7
+Fibonacci sequence:      
+0
+1
+1
+2
+3
+5
+8
+Avg is: 2.857142857142857
+
+
+#Output 8
+a part : {1, 2, 3, 4, 8, 9}
+b part : {1, 2, 3, 4, 67, 8, 9, 12, 77, 45}
+c part : {5, 7}
+d part : {1, 3, 6, 9, 10}
+e part is {10, 6}
