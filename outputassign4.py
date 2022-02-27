9c9d891e849939b7ce9a08a6185c7b914cd1c809
Pgarg2025 <97442147+Pgarg2025@users.noreply.github.com>
2022-02-27 20:07:31 +0530

    Add files via upload

---
+++ outputassign4.py
@@ -0,0 +1,98 @@
+#Output 1
+
+Transfer disk from A to C
+Transfer disk from A to B
+Transfer disk from C to B
+Transfer disk from A to C
+Transfer disk from B to A
+Transfer disk from B to C
+Transfer disk from A to C
+
+
+#Output 2
+#part a 
+Using iteration-
+Give the no. of rows-6
+      1  
+
+     1  1  
+
+    1  2  1  
+
+   1  3  3  1  
+
+  1  4  6  4  1  
+
+ 1  5  10  10  5  1  
+
+#part b 
+Using recursion-
+Please give no. of rows-6
+              1 
+
+             1 1 
+
+            1 2 1 
+
+           1 3 3 1 
+
+          1 4 6 4 1 
+
+         1 5 10 10 5 1 
+
+
+Done!
+
+
+#Output 3
+
+Please give the first number-73
+Please give the second number-7
+The quotient is- 10
+The remainder is- 3
+[10, 3]
+#part a
+Callable- True
+
+#part b
+a is zero- False
+b is zero- False
+quotient is zero- False
+remainder is zero- False
+
+#part c
+[10, 3, 4, 5, 6]
+The values greater than 4- [10, 5, 6]
+
+#part d
+{10, 5, 6}
+
+#part e
+The required immutable set frozenset({10, 5, 6})
+
+#part f
+The required max value is- 10
+The required hash value is- 10
+
+
+#Output 4
+Piyush
+21103103
+Destructor called,The object is destroyed.
+
+
+#Output 5
+ 
+#part a
+The updated salary of Mehak is- 70000
+
+#part b
+The record of Viren has been successfully deleted
+
+
+#Output 6
+Please give a word-xyz
+Give a word-yzx
+Possible Words- ['xyz', 'yxz', 'yzx', 'xzy', 'zxy', 'zyx']
+If Barbie's word lies in the list,then their friendship is real.
+Friendship is real.
